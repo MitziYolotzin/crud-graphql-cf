cf8dd0ab20a2d3de2258a1b3ee4159756124f5ca
MitziYolotzin <mitziulter@gmail.com>
2020-04-24 14:59:54 -0500

    add type query, json mocking

---
+++ courses.js
@@ -0,0 +1,11 @@
+module.exports = [{
+        id: "1",
+        title: 'Curso 1',
+        views: 1000
+    },
+    {
+        id: "2",
+        title: 'Curso 2',
+        views: 5000
+    }
+];
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { buildSchema } = require('graphql');
 
+const courses = require('./courses');
+
 const app = express();
 
 const schema = buildSchema(`
     type Course{
-        id: ID
-        title: String
+        id: ID!
+        title: String!
         views: Int
     }
-`)
+
+    type Query{
+        getCourses: [Course]
+
+    }
+`);
+//type Query, declarar todas las consultas que podemos hacer al serv de graphql
+
 
 app.get('/', function(req, res) {
     res.send("Hello");
