7a2cd901c35c9d658432423a2a781828eb73f77c
MitziYolotzin <mitziulter@gmail.com>
2020-04-24 15:15:43 -0500

    add root, comment desc code

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
 
 const courses = require('./courses');
 
 const app = express();
 
+//schema definition language
 const schema = buildSchema(`
     type Course{
         id: ID!
@@ -17,12 +19,26 @@ const schema = buildSchema(`
 
     }
 `);
-//type Query, declarar todas las consultas que podemos hacer al serv de graphql
+//type Query, all the queries we can make are declared to server in graphql
 
+//obj with functions, queries indicating that it returns
+const root = {
+    getCourses() {
+        return courses;
+    }
+}
 
 app.get('/', function(req, res) {
     res.send("Hello");
 });
+
+//middleware
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root, //works as initial value to res, of any query(consulta) in graphql
+    graphiql: true
+}));
+
 
 app.listen(8080, function() {
     console.log("Server on");
